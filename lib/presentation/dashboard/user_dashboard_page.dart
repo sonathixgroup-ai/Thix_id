4a167ac56fe71a864373de75d28b5f8b0eb8f43b
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-05-21 14:14:35 +0000

    Apply CodeRabbit fixes

--- lib/presentation/dashboard/user_dashboard_page.dart
+++ lib/presentation/dashboard/user_dashboard_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:typed_data';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
